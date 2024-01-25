576b40a198b078a7e50b50954a8e5ff5c3f90889
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2024-01-25 14:01:22 -0300

    alteradoURLCategory

--- src/App/models/Product.js
+++ src/App/models/Product.js
@@ -11,7 +11,7 @@ class Product extends Model {
             url: {
                 type: Sequelize.VIRTUAL, //campo virtual não será gravado
                 get(){ //mas quando der um get ele vai trazer a url e mostrar a imagem
-                    return `http://localhost:3000/product-file/${this.path}` //gera pegando esse endereço mais o nome do arquivo path    
+                    return `http://localhost:3001/product-file/${this.path}` //gera pegando esse endereço mais o nome do arquivo path    
                 }
             }
         },
