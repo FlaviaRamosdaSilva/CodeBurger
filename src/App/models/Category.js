ad249d46273768d3c7f9647be1ea111cfccfd000
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2024-02-14 18:51:00 -0300

    deploy-img

--- src/App/models/Category.js
+++ src/App/models/Category.js
@@ -8,7 +8,7 @@ class Category extends Model {
             url: {
                 type: Sequelize.VIRTUAL, //campo virtual não será gravado
                 get(){ //mas quando der um get ele vai trazer a url e mostrar a imagem
-                    return `http://localhost:3001/category-file/${this.path}` //gera pegando esse endereço mais o nome do arquivo path    
+                    return `postgresql://postgres:Ea*F5feD643E-4B2BfB6F5G6ad1eGG4d@monorail.proxy.rlwy.net:25535/railway/category-file/${this.path}` //gera pegando esse endereço mais o nome do arquivo path    
                 }
             }
         },
